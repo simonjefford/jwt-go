f90c351b4599be12dad9935d8e49140ccada08f4
Simon Jefford <simon.jefford@gmail.com>
2014-08-05 18:20:32 +0100

    jwk wip

--- jwk.go
+++ jwk.go
@@ -1,35 +1,31 @@
 package jwt
 
 import (
+	"crypto/rsa"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"math/big"
 	"net/http"
 )
 
 const (
-	HeaderKid = "kid"
+	HeaderKid                                 = "kid"
+	JWKStrategyModulusAndExponent JWKStrategy = iota
+	JWKStrategyCertificateURL
+	JWKStrategyCertificatePEM
+	JWKStrategyUnknown
 )
+
+type JWKStrategy int
 
 var (
-	ErrMissingKid = errors.New("No kid found")
-	ErrMissingKey = errors.New("No key found")
+	ErrMissingKid    = errors.New("No kid found")
+	ErrMissingKey    = errors.New("No key found")
+	ErrNoKeyStrategy = errors.New("Could not determine a strategy for retreiving a key")
 )
-
-type webKeyHTTPError struct {
-	url     string
-	message string
-}
-
-func (m webKeyHTTPError) Error() string {
-	msg := fmt.Sprintf("Failed to fetch a JSON Web Key Set from %s.", m.url)
-	if m.message != "" {
-		msg += fmt.Sprintf(" The server returned a message of \"%s\".", m.message)
-	}
-	return msg
-}
 
 type WebKey struct {
 	Kty     string `json:"kty"`
@@ -45,7 +41,87 @@ type WebKey struct {
 	E       string `json:"e"`
 }
 
+func (wks WebKey) keyRetrievalStrategy() JWKStrategy {
+	switch {
+	case wks.E != "" && wks.N != "":
+		return JWKStrategyModulusAndExponent
+	case wks.X5C != "":
+		return JWKStrategyCertificatePEM
+	case wks.X5U != "":
+		return JWKStrategyCertificateURL
+	default:
+		return JWKStrategyUnknown
+	}
+}
+
+type strategyTable map[JWKStrategy]func(WebKey) (interface{}, error)
+
+var strategies = strategyTable{
+	JWKStrategyModulusAndExponent: func(w WebKey) (interface{}, error) {
+		modBytes, err := DecodeSegment(w.N)
+		if err != nil {
+			return nil, err
+		}
+
+		expBytes, err := DecodeSegment(w.E)
+		if err != nil {
+			return nil, err
+		}
+
+		modInt := new(big.Int)
+		modInt.SetBytes(modBytes)
+
+		expInt := new(big.Int)
+		expInt.SetBytes(expBytes)
+
+		rsaKey := new(rsa.PublicKey)
+		rsaKey.N = modInt
+		rsaKey.E = int(expInt.Int64())
+		return rsaKey, nil
+	},
+	JWKStrategyCertificatePEM: func(w WebKey) (interface{}, error) {
+		key, err := DecodeSegment(w.X5C)
+		if err != nil {
+			return nil, err
+		}
+		return key, nil
+	},
+	JWKStrategyCertificateURL: func(w WebKey) (interface{}, error) {
+		res, err := http.Get(w.X5U)
+		if err != nil {
+			return nil, err
+		}
+		key, err := ioutil.ReadAll(res.Body())
+		if err != nil {
+			return nil, err
+		}
+		return key, nil
+	},
+}
+
+func (wks WebKey) Key() (interface{}, error) {
+	strategy := wks.keyRetrievalStrategy()
+	if strategy == JWKStrategyUnknown {
+		return nil, ErrNoKeyStrategy
+	}
+
+	return strategies[strategy](wks)
+}
+
 type WebKeySet []WebKey
+
+type webKeyHTTPError struct {
+	url     string
+	message string
+}
+
+func (m webKeyHTTPError) Error() string {
+	msg := fmt.Sprintf("Failed to fetch a JSON Web Key Set from %s.", m.url)
+	if m.message != "" {
+		msg += fmt.Sprintf(" The server returned a message of \"%s\".", m.message)
+	}
+	return msg
+}
 
 func GetWebKeySetFromURL(url string) (WebKeySet, error) {
 	res, err := http.Get(url)
@@ -89,10 +165,6 @@ func GetWebKeySetFromReader(reader io.Reader) (WebKeySet, error) {
 	return out.Keys, nil
 }
 
-func (wks WebKey) retrieveKey() (interface{}, error) {
-	return nil, nil
-}
-
 func (wks WebKeySet) KeyFunc(t *Token) (interface{}, error) {
 	kid := t.Kid()
 
@@ -102,7 +174,7 @@ func (wks WebKeySet) KeyFunc(t *Token) (interface{}, error) {
 
 	for _, wk := range wks {
 		if wk.Kid == kid {
-			return wk.retrieveKey()
+			return wk.Key()
 		}
 	}
 
